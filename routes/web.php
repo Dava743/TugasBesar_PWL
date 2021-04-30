8e79e1dcd6f00974d8335bd4ff30468fad2f33a6
Dava743 <davaprmudia743@gmail.com>
2021-04-30 23:52:22 +0700

    view laporan keuangan

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,11 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::group(['middleware' => ['auth']], function () {
+    Route::get(
+        '/',
+        [DashboardController::class, 'index']
+    )
         ->name('dashboard');
     /** Route Kasir **/
     Route::resource('kasir', CashierController::class);
@@ -19,9 +23,14 @@ use Illuminate\Support\Facades\Route;
     Route::resource('barang', ItemController::class);
     Route::resource('merek', MerkController::class);
     Route::resource('kategori', CategoriesController::class);
-    /** Route Profile **/
-    Route::resource('profil', ProfileController::class);
     Route::get(
         'laporan/barang/cetak',
         [ItemController::class, 'print']
     )->name('cetak.barang');
+    Route::get(
+        'laporan/keuangan/cetak',
+        [FinanceController::class, 'print']
+    )->name('cetak.keuangan');
+    /** Route Profile **/
+    Route::resource('profil', ProfileController::class);
+});
